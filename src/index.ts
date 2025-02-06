ccacb7512f9d2acccd05cdd8c84b78b033c9ffb4
chellysA <chellys.castillo@gmail.com>
2025-02-05 21:14:41 -0400

    feat: :zap: add componentts to src

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import Article from "./components/article";
+import ArticleList from "./components/article-list";
 
 customElements.define("article-component", Article);
+customElements.define("articles-list", ArticleList);
